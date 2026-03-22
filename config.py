d00e7d8e11e4cabd21f76c1213c51f41ea26442e
enyagboss <66272212+enyagboss@users.noreply.github.com>
2026-03-22 12:39:56 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 VK_TOKEN = "vk1.a.yvl38Ml6XZEa2allJ7OqkIKDO2O6cs79mkFP26cGdPgrkud-KEqm3pffRyiiy6qFm8CdoUWz4HiEpNHts8I_FgiNp_BA8-ikboubtDJCGgV1SRwo8k3a9m0lxSDgR9Ur2QZb1tZTmGrb1cIg6JYxs5KJCu-RHbuWp4Hm1FjQA4pUHUeyKnzE_HrkLMyE4CTQVYSfMgYiIH7HKFEvd3wpBw"       # токен сообщества VK
 GROUP_ID = 236907251                # ID вашей группы
 PSYCHOLOGIST_ID = 159256205        # ID пользователя (психолога) для уведомлений
-ADMIN_ID = 373422311                # ID администратора для логов (опционально)
+ADMIN_ID = 159256205                # ID администратора для логов (опционально)
 # Временные интервалы для цитат (можно настроить)
 QUOTE_TIMES = {
     "утром": "08:00",
